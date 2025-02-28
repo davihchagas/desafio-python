a861c2c3b4622290a7ff70d247d5644728f07beb
Davi Chagas <155915186+davihchagas@users.noreply.github.com>
2025-02-28 21:33:37 +0000

    Resolução operações matemáticas

---
+++ operacoes_matematicas.py
@@ -0,0 +1,9 @@
+# Solicita dois números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realiza uma operação matemática (soma)
+resultado = num1 + num2
+
+# Exibe o resultado
+print("Resultado da soma:", resultado)
